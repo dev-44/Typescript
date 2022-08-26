7ae558c5a28dfb4499d0a4491402e728fa7ebadf
plug47 <oarmoa44@gmail.com>
2022-08-26 09:10:50 -0400

    Type Aliases

--- objects.ts
+++ objects.ts
@@ -12,3 +12,19 @@ let coordinates: {x: number, y: number} = {x: 34, y:2}
 function randomCoordinate(): {x: number, y: number} {
     return {x: Math.random(), y: Math.random()}
 }
+
+//Type Aliases
+type Point = {
+    x: number,
+    y: number
+}
+
+let coordinate2: Point = {x: 34, y: 2}
+
+function randomCoordinate2(): Point {
+    return {x: Math.random(), y: Math.random()}
+}
+
+function doublePoint(point: Point): Point {
+    return {x: point.x * 2, y: point.y * 2}
+}
