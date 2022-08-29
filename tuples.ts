6c7fe13350a3b4dce7791ffd36bec7c6595df88a
plug47 <oarmoa44@gmail.com>
2022-08-29 14:21:07 -0400

    tuples

--- tuples.ts
+++ tuples.ts
@@ -1,3 +1,10 @@
 //Arrays with fixed lenght and types
 
 const color: [number, number, number] = [23, 45, 82]
+
+
+type HTTPResponse = [number, string]
+const goodResp: HTTPResponse = [200, "OK"] 
+
+//Array of tuples
+const responses: HTTPResponse[] = [[404, "Not Found"], [202, "OK"]]
